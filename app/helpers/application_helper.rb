a5ef79cd52a3bad7c9688bc3ee49e6c47f03af0b
ejdiener <64671864+ejdiener@users.noreply.github.com>
2022-09-18 11:59:54 -0400

    added the full_title helper method from ch4 of RORT

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = "Halal You Can Eat"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
