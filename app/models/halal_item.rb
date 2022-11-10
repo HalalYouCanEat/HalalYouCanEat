7a45223b7976344134d25fc6ebb67620d0b2047e
fritztheritz <fduverglas@brandeis.edu>
2022-11-10 12:57:04 -0500

    back to normal

--- app/models/halal_item.rb
+++ app/models/halal_item.rb
@@ -1,4 +1,4 @@
 class HalalItem < ApplicationRecord
 	belongs_to :restaurant
-	has_many :reviews, dependent: :destroy # changed from destroy to nullify
+	has_many :reviews, dependent: :nullify # changed from destroy to nullify
 end
