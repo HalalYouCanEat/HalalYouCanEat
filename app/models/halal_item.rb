7d8187f716e1369690f67f3a4c32e5800c090ff9
fritztheritz <fduverglas@brandeis.edu>
2022-11-10 12:50:46 -0500

    trying this again

--- app/models/halal_item.rb
+++ app/models/halal_item.rb
@@ -1,4 +1,4 @@
 class HalalItem < ApplicationRecord
 	belongs_to :restaurant
-	has_many :reviews, dependent: :delete_all # changed from destroy to nullify
+	has_many :reviews, dependent: :destroy # changed from destroy to nullify
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,4 @@
 class Restaurant < ApplicationRecord
-	has_many :reviews, dependent: :delete_all # changed from destroy to nullify
-	has_many :halal_items, dependent: :delete_all # changed from destroy to nullify
+	has_many :reviews, dependent: :destroy # changed from destroy to nullify
+	has_many :halal_items, dependent: :destroy # changed from destroy to nullify
 end
