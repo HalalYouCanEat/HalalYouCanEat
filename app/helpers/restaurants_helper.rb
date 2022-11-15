9b7f4797bb819d0579d55f1dd177520979fe30a1
fritztheritz <fduverglas@brandeis.edu>
2022-11-15 18:12:26 -0500

    added bootstrap into app and resized table

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -1,9 +1,9 @@
 module RestaurantsHelper
   def build_order_link(column:, label:)
     if column == session.dig('restaurant_filters', 'column')
-      link_to(label, list_restaurants_path(column: column, direction: next_direction))
+      link_to(label, list_restaurants_path(column:, direction: next_direction))
     else
-      link_to(label, list_restaurants_path(column: column, direction: 'asc'))
+      link_to(label, list_restaurants_path(column:, direction: 'asc'))
     end
   end
 
@@ -15,7 +15,11 @@ module RestaurantsHelper
     tag.span(class: "sort sort-#{session['restaurant_filters']['direction']}")
   end
 
-	def cuisine_collector
-		Restaurant.all.pluck(:cuisine).uniq.sort.map { |cuisine| [cuisine, cuisine] }
-	end
+  def cuisine_collector
+    Restaurant.all.pluck(:cuisine).uniq.sort.map { |cuisine| [cuisine, cuisine] }
+  end
+
+  def on_home_page
+    params[:action] != 'index' && params[:action] != 'search'
+  end
 end
