51d44e947bbd877076ea9830e6de81bf75c1991d
fritztheritz <fduverglas@brandeis.edu>
2022-11-15 13:48:38 -0500

    adding additional settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
 	config.action_mailer.smtp_settings = {
 		:address        => 'smtp.gmail.com',
     :port           => '587',
-    :authentication => :plain,
+    :authentication => "plain",
     :user_name      => 'halalyoucaneat',
     :password       => 'lzvrxttgkfijgbeo',
     :domain         => 'gmail.com',
