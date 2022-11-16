cdaf480f3e7f607dbde4ac80e81a5c6c51eefe53
syed-hassan-brandeis <syedhassan@brandeis.edu>
2022-11-15 20:44:56 -0500

    Added City Filter

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -19,6 +19,10 @@ module RestaurantsHelper
     Restaurant.all.pluck(:cuisine).uniq.sort.map { |cuisine| [cuisine, cuisine] }
   end
 
+  def city_collector
+    Restaurant.all.pluck(:city).uniq.sort.map { |city| [city, city] }
+  end
+
   def on_home_page
     params[:action] != 'index' && params[:action] != 'search'
   end
