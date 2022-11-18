d03cf8820ebb4e255305266ed6f7156f2274bf84
Anjola Uprety <anjolauprety@brandeis.edu>
2022-11-17 22:29:15 -0500

    added validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,5 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :halal_item
   validates :user_id, presence: true
-  validates :content, presence: true # length: { maximum: 140 }
+  validates :content, presence: true , length: { maximum: 140 }
 end
