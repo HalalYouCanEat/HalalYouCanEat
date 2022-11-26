e4e53aa727bf6d4f5e67690abc2c5a7e9d35b744
ejdiener <64671864+ejdiener@users.noreply.github.com>
2022-11-26 14:36:56 -0500

    13.2 DONE

--- app/views/restaurants/_restaurant.json.jbuilder
+++ app/views/restaurants/_restaurant.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! restaurant, :id, :id, :location_id, :name, :created_at, :updated_at
+json.extract! restaurant, :id, :id, :name, :created_at, :updated_at
 json.url restaurant_url(restaurant, format: :json)
---
+++ test/integration/restaurant_profile_test.rb
@@ -0,0 +1,24 @@
+require_relative '../test_helper'
+
+class RestaurantProfileTest < ActionDispatch::IntegrationTest
+  include ApplicationHelper
+
+  def setup
+    @restaurant = restaurants(:one)
+  end
+
+  test 'profile display' do
+    get restaurant_path(@restaurant)
+    assert_template 'restaurants/show'
+    assert_select 'title', full_title(@restaurant.name)
+    assert_select 'h2', text: "This is #{@restaurant.name}!"
+    assert_select 'h4', text: "We are located at #{@restaurant.address}."
+    # assert_select 'h4', text: "Our cuisine is #{@restaurant.cuisine}."
+    assert_select 'h4', text: "Our rating is #{@restaurant.rating}"
+    assert_match @restaurant.reviews.count.to_s, response.body
+    assert_select 'div.pagination'
+    @restaurant.reviews.paginate(page: 1).each do |review|
+      assert_match review.content, response.body
+    end
+  end
+end
