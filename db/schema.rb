20354252753462beae428cd9541215693b73f8c7
fritztheritz <fduverglas@brandeis.edu>
2022-11-28 17:20:09 -0500

    can only add to favorites when logged in

--- db/schema.rb
+++ db/schema.rb
@@ -84,6 +84,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_212924) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "reset_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
