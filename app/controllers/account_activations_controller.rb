1e622fa789d7f25e3ad778daa8520e836f96c34f
fritztheritz <fduverglas@brandeis.edu>
2022-12-08 20:00:57 -0500

    added extra case for activation

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -2,11 +2,15 @@ class AccountActivationsController < ApplicationController
   include(SessionsHelper)
   def edit
     user = User.find_by(email: params[:email])
+
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
       log_in user
       flash[:success] = 'Account activated!'
       redirect_to user
+		elsif user && user.activated? && user.authenticated?(:activation, params[:id])
+			flash[:info] = 'Please log in to activate your account'
+			redirect_to login_url
     else
       flash[:danger] = 'Invalid activation link'
       redirect_to root_url
